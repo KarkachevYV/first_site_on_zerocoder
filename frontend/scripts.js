826fd78f03d542c68b50518b03547b4071a52230
Karkach <Karkach_Y_V@outlook.com>
2025-01-10 17:50:46 +0300

    Знаменитый блокбастер

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,24 +1,9 @@
+// Пример скрипта для модального окна
 document.addEventListener('DOMContentLoaded', function() {
   var modal = document.getElementById('userModal');
-  var modalImage = document.getElementById('modalImage');
-  var modalName = document.getElementById('modalName');
-  var modalEmail = document.getElementById('modalEmail');
-  var modalBirth = document.getElementById('modalBirth');
-  var modalCity = document.getElementById('modalCity');
-  var span = document.getElementsByClassName('close')[0];
+  var closeModal = document.getElementsByClassName('close')[0];
 
-  document.querySelectorAll('.user-card').forEach(card => {
-      card.addEventListener('click', function() {
-          modal.style.display = 'block';
-          modalImage.src = this.querySelector('img').src;
-          modalName.textContent = this.getAttribute('data-name');
-          modalEmail.textContent = 'Email: ' + this.getAttribute('data-email');
-          modalBirth.textContent = 'Дата рождения: ' + this.getAttribute('data-birth');
-          modalCity.textContent = 'Город: ' + this.getAttribute('data-city');
-      });
-  });
-
-  span.onclick = function() {
+  closeModal.onclick = function() {
       modal.style.display = 'none';
   }
 
