56c0d9db0ec34e1b6d03bc9b5828e706c48f8348
Karkach <Karkach_Y_V@outlook.com>
2025-01-15 07:13:45 +0300

    Моё первое Flask приложение

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Получаем текущие дату и время
-    now = datetime.now()
-    # Форматируем их для отображения
-    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    # Возвращаем как HTML-страницу
-    return f"<h1>Текущие дата и время: {current_time}</h1>"
+def index():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
